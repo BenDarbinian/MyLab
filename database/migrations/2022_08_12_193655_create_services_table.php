56cfbacf178136984179f72a2dfbd3745d16fa77
BenDarbinian <bendarbinian@gmail.com>
2022-08-12 23:06:09 +0300

    first api with get

--- database/migrations/2022_08_12_193655_create_services_table.php
+++ database/migrations/2022_08_12_193655_create_services_table.php
@@ -15,6 +15,10 @@ return new class extends Migration
     {
         Schema::create('services', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('slug')->unique();
+            $table->integer('price');
+            $table->integer('time');
             $table->timestamps();
         });
     }
